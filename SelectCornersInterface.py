64cdf2a5f398d6c715e51ad80233bc4fb00efdb7
Sander van Bennekom <s.vanbennekom1@students.uu.nl>
2023-02-14 11:15:42 +0100

    Small adjustments to text size

--- SelectCornersInterface.py
+++ SelectCornersInterface.py
@@ -1,9 +1,11 @@
 import cv2
 import numpy as np
+import math
 
 
 class SelectCornersInterface:
     def __init__(self, path):
+        self.path = path
         self.img = cv2.imread(path)
         self.font = cv2.FONT_HERSHEY_SIMPLEX
         self.click = 0
@@ -12,7 +14,7 @@ class SelectCornersInterface:
 
     def show_img(self):
         # Printing the instructions for the user on the image
-        text = "Select the four corner points with the left mouse button"
+        text = "Select corners"
         t_size = cv2.getTextSize(text, self.font, 2, 4)
         text_x = int((self.img.shape[1] - t_size[0][0]) / 2)
         text_y= int((self.img.shape[0] + t_size[0][1]) / 2)
@@ -43,7 +45,7 @@ class SelectCornersInterface:
             self.corners[self.click - 1] = corner
 
             # Displaying the points on the image window
-            cv2.circle(self.img, (x, y), 15, (0, 0, 255), -1)
+            cv2.circle(self.img, (x, y), 10, (0, 0, 255), -1)
             cv2.imshow('image', self.img)
 
             # Closing the interface after four points have been selected
@@ -52,8 +54,10 @@ class SelectCornersInterface:
                 cv2.destroyAllWindows()
 
     def show_corners(self):
+        self.img = cv2.imread(self.path)
         # Writing the coordinates of each corner on the image
         for corner in self.corners:
+            cv2.circle(self.img, (corner[0], corner[1]), 10, (0, 0, 255), -1)
             cv2.putText(self.img, str(corner[0]) + ',' +
                         str(corner[1]), (corner[0], corner[1]), self.font,
                         1, (255, 0, 0), 2)
