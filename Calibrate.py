3cb6fb040bdd04382eb5dcabfdf306c9543aceef
Sander van Bennekom <s.vanbennekom1@students.uu.nl>
2023-02-16 12:50:20 +0100

    -

--- Calibrate.py
+++ Calibrate.py
@@ -41,29 +41,3 @@ def calibrate(image_names, board_shape):
                                                                                     gray.shape[::-1], None, None)
 
     return camera_matrix, distortion_coef, rot_vecs, trans_vecs
-
-from Calibrate import calibrate
-import glob
-from SelectCornersInterface import SelectCornersInterface
-
-# Collecting filenames
-
-# 25 images for which the findChessboardCorners function is succesful
-images_auto = glob.glob("C:/Users/svben/PycharmProjects/pythonProject/Schaakbord_fotos/WhatsApp*")
-
-# 5 images for which the findChessboardCorners function is not succesful
-images_manual = glob.glob("C:/Users/svben/PycharmProjects/pythonProject/Schaakbord_fotos/Manual*")
-
-# test image
-test_img = glob.glob("C:/Users/svben/PycharmProjects/pythonProject/Schaakbord_fotos/test.jpeg")
-
-# Run 1
-camera_matrix_1, distortion_coef_1, rot_vecs_1, trans_vecs_1 = calibrate(images_manual+images_manual)
-
-# Run 2
-selection_1 = images_auto[0:10]
-camera_matrix_2, distortion_coef_2, rot_vecs_2, trans_vecs_2 = calibrate(selection_1)
-
-# Run 3
-selection_2 = images_auto[0:5]
-camera_matrix_3, distortion_coef_3, rot_vecs_3, trans_vecs_3 = calibrate(selection_2)
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-import cv2 as cv
-import numpy as np
+from Calibrate import calibrate
+import glob
 from SelectCornersInterface import SelectCornersInterface
 
-# Selecting the corners manually
-# IF = SelectCornersInterface("C:/Users/svben/PycharmProjects/pythonProject/Schaakbord_fotos/Manual_1.jpeg")
-# "C:/Users/svben/PycharmProjects/pythonProject/Schaakbord_fotos/Schaakbord_1.jpeg"
-IF = SelectCornersInterface("/Users/macbook/Desktop/testfoto.jpg")
+# Collecting filenames
 
-# Showing the selected corners on the image
-IF.show_corners2()
+# 25 images for which the findChessboardCorners function is succesful
+images_auto = glob.glob("C:/Users/svben/PycharmProjects/pythonProject/Schaakbord_fotos/WhatsApp*")
 
-# Printing the coordinates of the corners
-print(IF.corners)
+# 5 images for which the findChessboardCorners function is not succesful
+images_manual = glob.glob("C:/Users/svben/PycharmProjects/pythonProject/Schaakbord_fotos/Manual*")
 
+# test image
+test_img = glob.glob("C:/Users/svben/PycharmProjects/pythonProject/Schaakbord_fotos/test.jpeg")
+
+# Run 1
+camera_matrix_1, distortion_coef_1, rot_vecs_1, trans_vecs_1 = calibrate(images_manual+images_manual)
+
+# Run 2
+selection_1 = images_auto[0:10]
+camera_matrix_2, distortion_coef_2, rot_vecs_2, trans_vecs_2 = calibrate(selection_1)
+
+# Run 3
+selection_2 = images_auto[0:5]
+camera_matrix_3, distortion_coef_3, rot_vecs_3, trans_vecs_3 = calibrate(selection_2)
